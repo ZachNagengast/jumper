dd77046cd04a3a384ce22842165db614d17c65a5
Zach Nagengast <zachnagengast09@gmail.com>
2013-07-29 18:26:30 -0700

    cleaned the slate

--- background.js
+++ background.js
@@ -3,20 +3,19 @@
 // found in the LICENSE file.
 
 // Called when the user clicks on the browser action.
-var scrollToggle = 1;
 
 chrome.browserAction.onClicked.addListener(function(tab) {
   // No tabs or host permissions needed!
-  if(!localStorage["scrollSpeed"]){localStorage["scrollSpeed"]=50;}
-  if (scrollToggle) {
-  console.log('Turning ' + tab.url + ' red!');
-  localStorage["intervalID"] = setInterval(function startScroll() {chrome.tabs.executeScript({
-    code: 'document.body.scrollTop +=1'
-  });},localStorage["scrollSpeed"]);
-  console.log("Scrolling: "+localStorage["scrollSpeed"]);
-  scrollToggle = 0;
-}else{
-	window.clearInterval(localStorage["intervalID"]);
-	scrollToggle = 1; 
-}
+//   if(!localStorage["scrollSpeed"]){localStorage["scrollSpeed"]=50;}
+//   if (scrollToggle) {
+//   console.log('Turning ' + tab.url + ' red!');
+//   localStorage["intervalID"] = setInterval(function startScroll() {chrome.tabs.executeScript({
+//     code: 'document.body.scrollTop +=1'
+//   });},localStorage["scrollSpeed"]);
+//   console.log("Scrolling: "+localStorage["scrollSpeed"]);
+//   scrollToggle = 0;
+// }else{
+// 	window.clearInterval(localStorage["intervalID"]);
+// 	scrollToggle = 1; 
+// }
 });
